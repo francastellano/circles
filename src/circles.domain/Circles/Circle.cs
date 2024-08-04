484de89bf8276a03ab1a9989b34429275ffe05e7
Francisco Castellano <francisco.castellano@zolva.es>
2024-08-04 10:01:34 +0200

    Avoid warning

--- src/circles.domain/Circles/Circle.cs
+++ src/circles.domain/Circles/Circle.cs
@@ -3,5 +3,5 @@ namespace circles.domain.Circles;
 public sealed class Circle
 {
     public Guid Id { get; set; }
-    public string Denomination { get; set; }
+    public string Denomination { get; set; } = string.Empty;
 }
