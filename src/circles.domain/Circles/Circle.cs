165be7931e1bed37b1e1a18f638258ea5fa1be24
Francisco Castellano <francisco.castellano@zolva.es>
2024-08-16 09:08:28 +0200

    Add new constructor

--- src/circles.domain/Circles/Circle.cs
+++ src/circles.domain/Circles/Circle.cs
@@ -5,10 +5,16 @@ public sealed class Circle
 
     internal Circle() { }
 
-    public Circle(string denomination)
+    internal Circle(string denomination)
     {
         Denomination = denomination;
     }
     public Guid Id { get; set; }
     public string Denomination { get; set; } = string.Empty;
+
+    public static Circle Create(string denomination)
+    {
+        Circle circle = new Circle(denomination);
+        return circle;
+    }
 }
