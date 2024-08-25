631b5ecc6eb96979f07a01a0a35050f4ca2699bf
Francisco Castellano <francisco.castellano@zolva.es>
2024-08-25 20:20:41 +0200

    Avoid hardcode url

--- src/circles/CustomAuthorizationMessageHandler.cs
+++ src/circles/CustomAuthorizationMessageHandler.cs
@@ -5,13 +5,20 @@ namespace circles;
 
 public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
 {
+
+
     public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
-        NavigationManager navigation)
+        NavigationManager navigation,
+        IConfiguration configuration)
         : base(provider, navigation)
     {
+
+        var authUrl = configuration.GetValue<string>("ApiSettings:BaseUri");
+        if (authUrl is null)
+            throw new InvalidOperationException("The configuration 'ApiSettings:BaseUri' value is null or empty");
+
         ConfigureHandler(
-            authorizedUrls: [
-                "http://localhost:5130", "https://api.circles4.com", ],
-            scopes: new[] { "https://Circles03.onmicrosoft.com/ab4704f3-1f2a-4d04-8569-58447cb8884d/User" });
+            authorizedUrls: [authUrl],
+            scopes: ["https://Circles03.onmicrosoft.com/ab4704f3-1f2a-4d04-8569-58447cb8884d/User"]);
     }
 }
