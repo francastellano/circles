313c43f7cc20a112af7e882ec6e4f3c5127afdbf
Francisco Castellano <francisco.castellano@zolva.es>
2024-08-25 20:23:36 +0200

    Avoid Harcode scopes

--- src/circles/CustomAuthorizationMessageHandler.cs
+++ src/circles/CustomAuthorizationMessageHandler.cs
@@ -5,20 +5,21 @@ namespace circles;
 
 public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
 {
-
-
     public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
-        NavigationManager navigation,
-        IConfiguration configuration)
+                                             NavigationManager navigation,
+                                             IConfiguration configuration)
         : base(provider, navigation)
     {
-
         var authUrl = configuration.GetValue<string>("ApiSettings:BaseUri");
         if (authUrl is null)
             throw new InvalidOperationException("The configuration 'ApiSettings:BaseUri' value is null or empty");
 
+        var authScope = configuration.GetValue<string>("AzureAdB2C:TokenScope");
+        if (authScope is null)
+            throw new InvalidOperationException("The configuration 'AzureAdB2C:TokenScope' value is null or empty");
+
         ConfigureHandler(
             authorizedUrls: [authUrl],
-            scopes: ["https://Circles03.onmicrosoft.com/ab4704f3-1f2a-4d04-8569-58447cb8884d/User"]);
+            scopes: [authScope]);
     }
 }
