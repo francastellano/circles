470c3a3e06a65e1b8188a6b389c3ce88bfa8a808
Francisco Castellano <francisco.castellano@zolva.es>
2024-08-27 15:19:35 +0200

    Refactor

--- src/circles.application/Features/Goals/Commands/CircleGoalAddCommand.cs
+++ src/circles.application/Features/Goals/Commands/CircleGoalAddCommand.cs
@@ -9,17 +9,24 @@ namespace circles.application.Features.Goals.Commands;
 
 public sealed record CircleGoalAddCommand(CircleGoalAddRequest Params) : IRequest;
 
-internal sealed record CircleGoalAddCommandHandler(CirclesDbContext DbContext) : IRequestHandler<CircleGoalAddCommand>
+internal sealed record CircleGoalAddCommandHandler : IRequestHandler<CircleGoalAddCommand>
 {
+
+    private readonly CirclesDbContext _dbContext;
+
+    public CircleGoalAddCommandHandler(CirclesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
     public async Task Handle(CircleGoalAddCommand request, CancellationToken cancellationToken)
     {
-        var circle = await DbContext.Circles.FirstOrDefaultAsync(e => e.Id == request.Params.CircleId, cancellationToken: cancellationToken);
+        var circle = await _dbContext.Circles.FirstOrDefaultAsync(e => e.Id == request.Params.CircleId, cancellationToken: cancellationToken);
         if (circle is null)
             throw new ItemCantBeFoundException("Circle");
 
         var item = CircleGoal.Create(circle, request.Params.Denomination);
 
-        await DbContext.AddAsync(item, cancellationToken);
-        await DbContext.SaveChangesAsync(cancellationToken);
+        await _dbContext.AddAsync(item, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
     }
 }
